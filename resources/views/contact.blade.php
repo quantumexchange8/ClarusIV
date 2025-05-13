2fd2eb48f08c612b47bd4ca8e5eaafa0fa28a601
quantumexchange8 <96815929+quantumexchange8@users.noreply.github.com>
2025-05-13 16:33:23 +0800

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -239,7 +239,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xl-12">
-                <iframe src="https://www.google.com/maps/embed/v1/place?q=30+Herbert+St,+2nd+Floor,+Dublin,+D02+W329,+Ireland&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed/v1/search?q=32+Molesworth+Street,+Dublin,+D02+Y512,+Ireland &key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </div>
